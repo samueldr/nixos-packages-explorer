e659345b0eb74b609d7967e6689256ca017f223e
Samuel Dionne-Riel <samuel@dionne-riel.com>
2018-02-10 23:06:53 -0500

    Updates default.nix to add missing python.

--- default.nix
+++ default.nix
@@ -1,9 +1,10 @@
 with import <nixpkgs> {};
 stdenv.mkDerivation rec {
-	name = "nixos-packages-explorer-env";
-	# Makes those dependencies available in the environment.
-	buildInputs = [
-		nodejs-6_x           # Lock to 6.x
-		yarn                 # Much better than npm
-    ];
+  name = "nixos-packages-explorer-env";
+  # Makes those dependencies available in the environment.
+  buildInputs = [
+    nodejs-6_x           # Lock to 6.x
+    yarn                 # Much better than npm
+    python               # To build the json files
+  ];
 }
