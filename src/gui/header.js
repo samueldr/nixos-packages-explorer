dcfaeb7a43014ea5cc39dd6a1e00a5318214de63
Samuel Dionne-Riel <samuel@dionne-riel.com>
2018-04-02 22:07:37 -0400

    Refactors query/unfree into components.

--- src/gui/header.js
+++ src/gui/header.js
@@ -1,43 +1,14 @@
 import React from "react";
 import Channels from "./channels";
-import {use} from "../state";
+import Query from "./query";
+import Unfree from "./unfree";
 
-const Header = ({query, set_query, unfree, set_unfree}) =>
+const Header = () =>
 	<header>
 		<Channels />
-		<div>
-			<input
-				name="query"
-				type="text"
-				class="search-query span3"
-				placeholder="Search by name or description (regex allowed)"
-				id="search"
-				defaultValue={query}
-				onChange={(event) => set_query(event.target.value)}
-				onBlur={(event) => set_query(event.target.value, true)}
-			/>
-		</div>
-		<div class="form-checkbox">
-			<label>
-				<input
-					name="unfree"
-					type="checkbox"
-					checked={unfree}
-					onClick={(event) => set_unfree(event.target.checked)}
-				/><span>Show unfree packages</span>
-			</label>
-		</div>
+		<Query />
+		<Unfree />
 	</header>
 ;
 
-export default use(
-	[
-		"query",
-		"unfree",
-	],
-	[
-		"set_query",
-		"set_unfree",
-	],
-	Header
-);
+export default Header;
