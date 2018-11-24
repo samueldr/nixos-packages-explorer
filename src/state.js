c472344daa8256df1d096f9897f4e50eca866754
Samuel Dionne-Riel <samuel@dionne-riel.com>
2018-11-23 22:37:15 -0500

    (porting) channel generation changes.

--- src/state.js
+++ src/state.js
@@ -142,7 +142,7 @@ class State extends Component {
 	fetch_channels() {
 		this.setState({loading: this.state.loading + 1});
 
-		return fetch("channels/packages_channels.json", {mode: "cors"})
+		return fetch("/channels/packages-channels.json", {mode: "cors"})
 			.then((response) => response.json())
 			.then((channels) => {
 				this.setState({
@@ -163,7 +163,12 @@ class State extends Component {
 	fetch_channel() {
 		const {channel} = this.state;
 		this.setState({loading: this.state.loading + 1});
-		fetch(`channels/packages_${channel}.json`, {mode: "cors"})
+		const {hostname} = window.location;
+
+		// Some (most?) development environment don't really like to serve .gz files.
+		const dev = window.DEVELOPMENT || hostname === "localhost" || hostname === "127.0.0.1";
+		const ext = dev ? "json" : "json.gz";
+		fetch(`/channels/packages-${channel}.${ext}`, {mode: "cors"})
 			.then((response) => response.json())
 			.then((channel_data) => {
 				// Ensures we update only for the currently selected channel.
