93fc36834cf8bbb6a31b03ba5434ab8f80f1e0e5
Samuel Dionne-Riel <samuel@dionne-riel.com>
2018-11-23 22:38:30 -0500

    (porting) Channels selection as buttons.

--- src/gui/channels.js
+++ src/gui/channels.js
@@ -1,17 +1,53 @@
-import React from "react";
+import React, {Component} from "react";
 import {use} from "../state";
 
+/**
+ * Button component which shows up as *something else* than a classic radio
+ * button.
+ *
+ * The state is needed for the focus state.
+ */
+class Button extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {focus: false};
+	}
+
+	render() {
+		const {active, name, value, set_channel} = this.props;
+		const {focus} = this.state;
+
+		return (
+			<div class="radio-button">
+				<label
+					class={[
+						"btn",
+						active ? "active" : "not-active",
+						focus ? "focus" : "not-focus",
+					].join(" ")}
+				>
+					<input
+						type="radio"
+						name={name}
+						value={value}
+						checked={active}
+						onChange={(event) => set_channel(event.target.value)}
+						onFocus={() => this.setState({focus: true})}
+						onBlur={() => this.setState({focus: false})}
+					/>
+					<span>{value}</span>
+				</label>
+			</div>
+		);
+	}
+}
+
 const Channels = ({channels, channel, set_channel}) =>
-	<div>
-		<select
-			name="channel"
-			class="channels-select span3"
-			value={channel}
-			onChange={(event) => set_channel(event.target.value)}
-		>
-			<option disabled value="" selected={!channels}> — Select a channel — </option>
-			{channels.map((c) => <option key={c} value={c}>{c}</option>)}
-		</select>
+	<div class="channels-selector">
+		<label>Channel</label>
+		<div class="channels-selector--options">
+			{channels.map((c) => <Button key={c} name="channel" value={c} active={channel === c} set_channel={set_channel} />)}
+		</div>
 	</div>
 ;
 
